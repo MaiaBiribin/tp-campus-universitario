e507915497123b61f9c113b641694d369bcb634a
unknown <maiabiribin@hotmail.com>
2026-06-08 23:48:48 -0300

    endpoints de carreras y materias con protección por roles

--- backend/src/carreras/carreras.service.ts
+++ backend/src/carreras/carreras.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Carrera } from './carrera.entity';
 
 @Injectable()
-export class CarrerasService {}
+export class CarrerasService {
+
+  constructor(
+    @InjectRepository(Carrera)
+    private carrerasRepository: Repository<Carrera>,
+  ) {}
+
+  async findAll(): Promise<Carrera[]> {
+    return this.carrerasRepository.find();
+  }
+
+  async create(nombre: string): Promise<Carrera> {
+    const carrera = this.carrerasRepository.create({ nombre });
+    return this.carrerasRepository.save(carrera);
+  }
+}
