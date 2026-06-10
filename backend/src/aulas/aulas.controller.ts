a2479ba73080280adcc0036910c5519c765c6624
maiatcshop <maia.tcshop@gmail.com>
2026-06-10 11:23:13 -0300

    traer los cambios de backend desde maia

--- backend/src/aulas/aulas.controller.ts
+++ backend/src/aulas/aulas.controller.ts
@@ -1,21 +1,45 @@
-import { Controller, Get, Post, Put, Patch, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Delete, Body, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 
-@Controller('/')
+import { AulasService } from './aulas.service';
+
+@Controller('aulas')
 export class AulasController {
+
+  constructor(private readonly aulasService : AulasService) {}
+
+
+  // POST http://localhost:4000/aulas
   @ApiTags('Aulas')
-  @Get('/aulas/:id')
+  @ApiOperation({ summary: 'Crea una nueva aula, solo puede ser utilizado por el Administrador' })
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(['Admin'])
+  @Post()
+  createAulas(@Body() body: { nombre: string ; capacidad : number ; piso: number ; ubicacion : string }) {
+    return this.aulasService.create(body.nombre, body.capacidad, body.piso, body.ubicacion);
+  }
+
+  
+
+
+
+
+
+
+
+
+  
+  @ApiTags('Aulas')
   @ApiOperation({ summary: 'Devuelve lo que contenga el aula' })
+  @UseGuards(AuthGuard)
+  @Get('/aulas/:id')
   getAulas() {
     return 'aqui se verán las aulas';
   }
 
-  @ApiTags('Aulas')
-  @Post('/aulas')
-  @ApiOperation({ summary: 'Crea una nueva aula' })
-  createAulas() {
-    return '';
-  }
 
   @ApiTags('Aulas')
   @Patch('/aulas/:id')
--- backend/src/aulas/aulas.service.ts
+++ backend/src/aulas/aulas.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm';
+import {Aula} from './aula.entity';
 
 @Injectable()
-export class AulasService {}
+export class AulasService {
+
+    constructor(
+     @InjectRepository(Aula)
+     private aulasRepository: Repository<Aula>,
+    ){}
+
+    async findAll(): Promise<Aula[]> {
+        return this.aulasRepository.find();
+    }
+
+    async create(
+        nombre: string,
+        capacidad: number,
+        piso: number,
+        ubicacion: string,
+    ): Promise<Aula> {
+        const aula = this.aulasRepository.create({
+        nombre,
+        capacidad,
+        piso,
+        ubicacion,
+    });
+        return this.aulasRepository.save(aula);
+    }
+}
