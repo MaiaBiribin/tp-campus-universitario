8fd8a3d9352c1c1c867d12546132e49386e80f60
juli <julietacostenaro@gmail.com>
2026-06-13 00:30:05 -0300

    fix entidad materia y usuario

--- backend/src/materias/materias.module.ts
+++ backend/src/materias/materias.module.ts
@@ -3,18 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Materia } from './materia.entity';
-import { AlumnoMateria } from './alumno-materia.entity';
-import { DocenteMateria } from './docente-materia.entity';
 import { MateriasService } from './materias.service';
 import { MateriasController } from './materias.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Materia, AlumnoMateria, DocenteMateria]),
+    TypeOrmModule.forFeature([
+      Materia,
+    ]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+      useFactory: (
+        configService: ConfigService
+      ) => ({
+        secret:
+          configService.get<string>(
+            'JWT_SECRET'
+          ),
       }),
       inject: [ConfigService],
     }),
