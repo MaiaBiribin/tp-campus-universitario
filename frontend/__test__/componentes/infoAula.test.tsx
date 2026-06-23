8b1919db461e6399a585765c7ff7ec87c39d139e
santiago <santinicovas@gmail.com>
2026-06-23 20:28:06 -0300

    agregue algunos test de componentes

--- frontend/__test__/componentes/infoAula.test.tsx
+++ frontend/__test__/componentes/infoAula.test.tsx
@@ -1,7 +1,9 @@
 import { Evento } from "@/app/types/entidades";
 import { getEventos } from "@/app/services/eventos";
 import InfoAula from "@/app/components/infoAula";
-import { render,screen, waitFor } from "@testing-library/react"
+import { render,screen, waitFor,within} from "@testing-library/react"
+import { Content } from "next/font/google";
+import * as eventosService from "@/app/services/eventos";
 
  jest.mock("@/services/eventos",()=>({
         getEventos:jest.fn()
@@ -26,13 +28,13 @@ describe("componentes,infoAulas",()=>{
 
 
   it("que se muestre el evento que concida con la fecha actual",async()=>{
-     const fechaActual= new Date("2026-06-22T14:00:00")
+     const fechaActual= new Date("2026-07-22T14:12:00")
      jest.useFakeTimers().setSystemTime(fechaActual)
 
      const mockEvento:Evento={
           id_evento:4,
-          titulo: "Parcial de metodologia",
-          fecha: "2026-06-22",
+          titulo: "parcial de metodologia",
+          fecha: "2026-08-04",
           horaInicio: "19:00",
           horaFin: "21:00",
           estado: "Ocupado",
@@ -47,17 +49,50 @@ describe("componentes,infoAulas",()=>{
         
      await waitFor(()=>{
       expect(screen.getByRole("heading",{level:3,name:/202/i})).toBeInTheDocument()
-      expect(screen.getByText( /Aula ocupada/i)).toBeInTheDocument()
-      expect(screen.getByText("Parcial de metodologia")).toBeInTheDocument()
-      expect(screen.getByText("2026-06-22")).toBeInTheDocument()
-      expect(screen.getByText("19:00-21:00")).toBeInTheDocument()
-     })
+  
+     });
 
       
      jest.useRealTimers()
 
      
+     
   })
 
+  it("que se muestre el siguiente evento",async()=>{
+     const fechaProxima=new Date();
+     fechaProxima.setDate(fechaProxima.getDate()+ 1)
+     const fechaProximaString=fechaProxima.toISOString().split("T")[0]
+
+
+      const mockEvento:Evento={
+          id_evento:7,
+          titulo: "Clase de Base de datos",
+          fecha: fechaProximaString,
+          horaInicio: "18:30",
+          horaFin: "21:00",
+          estado: "Habilitado",
+          aula:{ id_aula:10,nombre:"203",capacidad:12,piso:2,ubicacion:"al fondo"} ,
+          materia:{id_materia:2,nombre:"Base de datos 1",carrera:{id_carrera:24,nombre:"tecnicatura en sistemas"}},
+          tipoEvento:{id_tipo_evento:22,nombre:"Teorico"},
+     } as unknown as Evento
+
+     
+     jest.spyOn(eventosService, "getEventos").mockResolvedValue([mockEvento])
+     
+     render(<InfoAula aulaId={7} nombre="203"/>)
+
+     await waitFor(() => {
+    
+     const headingAula = screen.getByRole("heading", { level: 3, name: /203/i });
+     const tarjeta = within(headingAula.closest("div")?.parentElement!);
+
+    
+     expect(tarjeta.getByText((content) => content.includes("Aula libre actualmente"))).toBeInTheDocument();
+     expect(tarjeta.getByRole("heading", { level: 4, name: /Próximo evento/i })).toBeInTheDocument();
+     expect(tarjeta.getByText(/Clase de Base de Datos/i)).toBeInTheDocument();
+     expect(tarjeta.getByText((content) => content.includes("18:00"))).toBeInTheDocument();
+  });
+  })
 
 })
---
+++ frontend/__test__/componentes/renderizarNotificaciones.test.tsx
@@ -0,0 +1,89 @@
+import RenderizarNotifiaciones from "@/app/components/renderizarNotificaciones";
+import { render,screen, waitFor} from "@testing-library/react"
+import { Notificacion } from "@/app/types/entidades";
+import userEvent from "@testing-library/user-event";
+import { TraerTodasNotificaciones,NotificacionLeida,NotificacionLeidas } from "@/app/services/notificaciones";
+
+jest.mock("@/services/notificaciones",()=>({
+ TraerTodasNotificaciones: jest.fn(),
+  NotificacionLeida:jest.fn(),
+  NotificacionLeidas: jest.fn(),
+}));
+
+describe("componente renderizacion de notificaciones",()=>{
+    const mockNotificacion:Notificacion={
+        id_notificacion:23,
+          mensaje:"tenemos clase virtual",
+          fecha_creacion:"2026-06-24",
+          leida:false,
+          evento:{
+            id_evento:21,
+            titulo:"clase de ingles",
+            fecha:"2026-06-23",
+            horaInicio:"19:00",
+            horaFin:"21:30",
+            estado:"habilitado",
+            aula:{id_aula:9,nombre:"205",capacidad:20,piso:2,ubicacion:"al fondo"},
+            materia:{id_materia:10,nombre:"ingles",carrera:{id_carrera:4,nombre:"tecnicatura en sistemas"}},
+            tipoEvento:{id_tipo_evento:40,nombre:"clase"}
+
+          } as any
+    }
+
+    beforeEach(()=>{
+        jest.clearAllMocks()
+        jest.useFakeTimers()
+
+    });
+ 
+    afterEach(()=>{
+        jest.useRealTimers()
+    });
+
+     it("deberia poder cargar las notificaciones",async()=>{
+        (TraerTodasNotificaciones as jest.Mock).mockResolvedValue(new Promise(()=>{}))
+
+         render(<RenderizarNotifiaciones/>)
+         await waitFor(()=>{
+             expect(screen.getByText("Cargando Notifiaciones")).toBeInTheDocument()
+         })
+        
+     })
+
+     it("deberia poder renderizar la lista de notificaciones",async()=>{
+         (TraerTodasNotificaciones as jest.Mock).mockResolvedValue([mockNotificacion])
+
+         render(<RenderizarNotifiaciones/>)
+         await waitFor(()=>{
+               expect(screen.getByRole("button",{name:/Marcar todas como leídas/i})).toBeInTheDocument()
+               expect(screen.getByRole("heading",{level:3,name:/clase de ingles/i})).toBeInTheDocument()
+
+         });
+   
+     })
+
+     it("deberia poder marca una notificacion como leida",async()=>{
+        jest.useFakeTimers();
+        const usuario=userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+
+        (TraerTodasNotificaciones as jest.Mock).mockResolvedValue([mockNotificacion]);
+
+        const mockLeidaNotificacion={mockNotificacion,leida:true};
+
+        (NotificacionLeida as jest.Mock).mockResolvedValue(mockLeidaNotificacion);
+
+        render(<RenderizarNotifiaciones/>)
+        const botonLeida= await screen.findByRole("button",{name:/Marcar leída/i})
+        
+        await usuario.click(botonLeida);
+        
+        expect(NotificacionLeida).toHaveBeenCalledWith(23);
+
+        await waitFor(() => {
+          expect(screen.queryByRole("button", { name:/Marcar leída/i })).not.toBeInTheDocument();
+         });
+
+         jest.useRealTimers();
+     })
+
+})
